40c89f9d9685eea0a17d2ec4854f76df766d9e16
Ryan Valle <ryanvalle@me.com>
2015-01-11 19:50:21 -0800

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150112001221) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: true do |t|
     t.string   "game"
     t.integer  "user_id"
